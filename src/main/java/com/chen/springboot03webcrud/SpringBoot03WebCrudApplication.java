b3b5d4d9986618dff4f328dd3fe7a5cc6c30a703
Winter-Lu <1606910199@qq.com>
2020-12-04 21:02:08 +0800

    Initial commit

---
+++ src/main/java/com/chen/springboot03webcrud/SpringBoot03WebCrudApplication.java
@@ -0,0 +1,13 @@
+package com.chen.springboot03webcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot03WebCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot03WebCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chen/springboot03webcrud/SpringBoot03WebCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chen.springboot03webcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot03WebCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
